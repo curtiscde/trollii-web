19303142da913f63affdf887b1e99504d7cd5a3d
Curtis Timson <curt@live.co.uk>
2018-03-11 19:01:15 +0000

    Display picture and nickname

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,56 +1,72 @@
 var lock = new Auth0Lock('pDx70JLEPV6h0w4Bxjhq8xzV8Lgmt7xK', 'curt.auth0.com',{
     languageDictionary: {"title":"Trollii"},
     allowedConnections: ["Username-Password-Authentication","google-oauth2"],
-    rememberLastLogin: false
+    rememberLastLogin: false,
+    auth: {
+        responseType: 'id_token token',
+        params: {
+          scope: 'openid profile',
+          audience: 'https://trollii.com/'
+        }
+    }
 });
 
 (function(){
 
-    lock.on("authenticated", function(authResult) {
-        // Use the token in authResult to getUserInfo() and save it to localStorage
-        lock.getUserInfo(authResult.accessToken, function(error, profile) {
-          if (error) {
-            // Handle error
-            return;
-          }
-      
-          document.getElementById('nick').textContent = profile.nickname;
-      
-          localStorage.setItem('accessToken', authResult.accessToken);
-          localStorage.setItem('profile', JSON.stringify(profile));
-        });
-    });
+    
 
 
 
     angular.module('trollii-web', [])
+    .config(function($interpolateProvider){
+        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+    })
     .controller('mainController', function($scope){
 
-    })
-    .controller('signupController', function($scope, $http){
-      
-        $scope.signup = function(serviceUrl){
+        $scope.isAuthenticated = false;
+        console.log('$scope.isAuthenticated', $scope.isAuthenticated);
 
-            // webAuth.authorize();
+        lock.on("authenticated", function(authResult) {
 
-            lock.show(function(err, profile, token) {
-                if (err) {
-                  // Error callback
-                  console.error("Something went wrong: ", err);
-                } else {
-                  // Success calback  
-
-                  console.log('token', token);
-                  console.log('userProfile', JSON.stringify(profile));
-            
-                  // Save the JWT token.
-                  localStorage.setItem('userToken', token);
-                  // Save the profile
-                  localStorage.setItem('userProfile', JSON.stringify(profile));
-                }
+            // Use the token in authResult to getUserInfo() and save it to localStorage
+            lock.getUserInfo(authResult.accessToken, function(error, profile) {
+              if (error) {
+                // Handle error
+                return;
+              }
+          
+              localStorage.setItem('accessToken', authResult.accessToken);
+              localStorage.setItem('profile', JSON.stringify(profile));
+    
+              console.log(authResult.accessToken);
+              console.log(profile);
             });
+        });
 
-        };
+        if (localStorage.getItem('accessToken')){
+            lock.getUserInfo(localStorage.getItem('accessToken'), function(error, profile) {
+                if (error) {
+                // Handle error
+                return;
+                }
+
+                console.log(profile);
+
+                $scope.isAuthenticated = true;
+                console.log('$scope.isAuthenticated', $scope.isAuthenticated);
+
+                $scope.profile = {
+                    picture: profile.picture,
+                    nickname: profile.nickname
+                };
+
+                $scope.$apply();
+            });
+        }
+
+        $scope.login = function(){
+            lock.show();
+        }
 
     });
 
