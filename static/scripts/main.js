a57eb5635145bb4321c26f5eb1849137d0a33da5
Curtis Timson <curt@live.co.uk>
2018-03-11 11:47:37 +0000

    Add intial Auth0 lock integration

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,27 +1,53 @@
+var lock = new Auth0Lock('pDx70JLEPV6h0w4Bxjhq8xzV8Lgmt7xK', 'curt.auth0.com',{
+    languageDictionary: {"title":"Trollii"},
+    allowedConnections: ["Username-Password-Authentication","google-oauth2"],
+    rememberLastLogin: false
+});
+
 (function(){
 
+    lock.on("authenticated", function(authResult) {
+        // Use the token in authResult to getUserInfo() and save it to localStorage
+        lock.getUserInfo(authResult.accessToken, function(error, profile) {
+          if (error) {
+            // Handle error
+            return;
+          }
+      
+          document.getElementById('nick').textContent = profile.nickname;
+      
+          localStorage.setItem('accessToken', authResult.accessToken);
+          localStorage.setItem('profile', JSON.stringify(profile));
+        });
+    });
+
+
+
     angular.module('trollii-web', [])
-    .controller('mainController', function(){
-       
-        
+    .controller('mainController', function($scope){
 
     })
     .controller('signupController', function($scope, $http){
       
         $scope.signup = function(serviceUrl){
 
-            console.log($scope.email);
+            // webAuth.authorize();
 
-            var data = {
-                email: $scope.email,
-                password: $scope.password
-            };
+            lock.show(function(err, profile, token) {
+                if (err) {
+                  // Error callback
+                  console.error("Something went wrong: ", err);
+                } else {
+                  // Success calback  
+
+                  console.log('token', token);
+                  console.log('userProfile', JSON.stringify(profile));
             
-            $http.post(serviceUrl + 'api/auth/signup', data)
-            .then(function(data){
-                console.log('success', data);   
-            }, function(){
-                console.log('error');
+                  // Save the JWT token.
+                  localStorage.setItem('userToken', token);
+                  // Save the profile
+                  localStorage.setItem('userProfile', JSON.stringify(profile));
+                }
             });
 
         };
