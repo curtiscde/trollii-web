edb1b151f6f2ced49ca60b84b5cbae48f64b1c0d
Curtis Timson <curt@live.co.uk>
2018-03-29 22:54:15 +0100

    Add routing to addlist or first list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,16 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 
 import { AuthService } from '../auth.service';
+import { ListService } from '../list.service';
+
+import { List } from '../models/list';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ListService]
 })
 export class HomeComponent implements OnInit {
 
   constructor(
-    public authService: AuthService,
+    private authService: AuthService,
+    private listService: ListService,
     private router: Router
   ) {}
 
@@ -19,6 +24,27 @@ export class HomeComponent implements OnInit {
     if (!this.authService.isAuthenticated()){
       this.router.navigate(['/login']);
     }
+    else {
+      this.getFirstList(() => {
+        if (this.list){
+          this.router.navigate(['/list', this.list._id]);
+        }
+        else{
+          this.router.navigate(['/addlist']);
+        }
+      });
+      
+    }
+  }
+
+  list: List;
+
+  getFirstList(cb) {
+    this.listService.getLists()
+      .subscribe(data => {
+        this.list = data[0];
+        cb();
+      }
   }
 
 }
