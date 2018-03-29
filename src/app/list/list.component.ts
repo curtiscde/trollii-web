f033e87b21abc54845d16040f6a039c2adf8c1de
Curtis Timson <curt@live.co.uk>
2018-03-29 23:20:39 +0100

    Display items from list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { AuthService } from '../auth.service';
+import { ListService } from '../list.service';
+
+import { List } from '../models/list';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [
+    ListService
+  ]
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private listService: ListService
+  ) { }
 
   ngOnInit() {
+    this.getList();
+  }
+
+  list: List;
+
+  getList() {
+    this.listService.getLists()
+      .subscribe(data => {
+        this.list = data.find(list => list._id === this.route.snapshot.paramMap.get('id'))
+      })
   }
 
 }
