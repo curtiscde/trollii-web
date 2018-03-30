6391bd736b0136ee0d15270e38e1175f2caca2df
Curtis Timson <curt@live.co.uk>
2018-03-30 14:37:17 +0100

    Add item

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -6,6 +6,7 @@ import { MatSnackBar } from '@angular/material';
 
 import { AuthService } from '../auth.service';
 import { ListService } from '../list.service';
+import { ItemService } from '../item.service';
 
 import { List } from '../models/list';
 
@@ -14,7 +15,8 @@ import { List } from '../models/list';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
   providers: [
-    ListService
+    ListService,
+    ItemService
   ]
 })
 export class ListComponent implements OnInit {
@@ -23,6 +25,7 @@ export class ListComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private listService: ListService,
+    private itemService: ItemService,
     public snackBar: MatSnackBar
   ) { }
 
@@ -46,13 +49,22 @@ export class ListComponent implements OnInit {
   }
 
   removeList() {
-    console.log(`remove list ${this.list._id}`);
     this.listService.deleteList(this.list)
       .subscribe(data => {
         this.snackBar.open(`List "${this.list.name}" deleted`, '', {
           duration: 1000
         });
         this.router.navigate(['']);
+      });
+  }
+
+  addItem(name: string){
+    this.itemService.addItem(this.list._id, name)
+      .subscribe(data => {
+        this.snackBar.open(`Item "${name}" added`, '', {
+          duration: 1000
+        });
+        this.list = data;
       });
   }
 
