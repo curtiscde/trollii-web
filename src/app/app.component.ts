8712fabec9af228d5182b7f2708bd6e8a9feb237
Curtis Timson <curt@live.co.uk>
2018-04-01 17:10:50 +0100

    Control sidebar via service and close on logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 
 import { AuthService } from './auth.service';
+import { SidebarService } from './sidebar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SidebarService]
 })
 export class AppComponent {
   title = 'app';
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    public sidebarService: SidebarService
   ) {}
   
   ngOnInit() {
+  }
+
+  sidebarOpened(){
+    return this.sidebarService.opened;
   }
 
   isAuthenticated() {
