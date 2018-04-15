ac8b718a11564168a9edc7886697f8b053754bd4
Curtis Timson <curt@live.co.uk>
2018-04-15 15:46:36 +0100

    Add google analytics tracking

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   serviceUrl: 'https://trollii-prod.herokuapp.com',
+  googleAnalyticsKey: 'UA-116772216-2',
   auth0: {
     domain: 'curt.auth0.com',
     clientId: 'OUaEBHkqGtDklsNqkheqVCxRhWXjBreF',
